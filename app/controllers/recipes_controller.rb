1b35bfe50e35533caa1db1746aa82e4b07a36a92
Hazel Chui <hazelchui@uchicago.edu>
2023-05-24 16:57:17 +0000

    Only user can generate

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -32,58 +32,64 @@ class RecipesController < ApplicationController
     render({ :template => "recipes/show.html.erb" })
   end
 
+
   def create
     the_recipe = Recipe.new
-    the_recipe.user_id = @current_user.id
-    the_recipe.ingredient = params.fetch("query_ingredient")
+    if @current_user != nil 
+      the_recipe.user_id = @current_user.id
 
-    if the_recipe.valid?
-      the_recipe.save
+      the_recipe.ingredient = params.fetch("query_ingredient")
 
-      system_message = Message.new
-      system_message.recipe_id = the_recipe.id
-      system_message.role = "system"
-  
-      system_message.content =  "You are a chef. The user has #{the_recipe.ingredient} left in the fridge. Suggest a recipe that includes #{the_recipe.ingredient}. Modify the recipe based on the user's additional request. The recipe should include a section of '#Ingredients:' and a section of 'Instructions:'. At the end of the recipe, say 'Bon Appétit!' " 
+      if the_recipe.valid?
+        the_recipe.save
 
-      system_message.save
+        system_message = Message.new
+        system_message.recipe_id = the_recipe.id
+        system_message.role = "system"
+    
+        system_message.content =  "You are a chef. The user has #{the_recipe.ingredient} left in the fridge. Suggest a recipe that includes #{the_recipe.ingredient}. Modify the recipe based on the user's additional request. The recipe should include a section of '#Ingredients:' and a section of 'Instructions:'. At the end of the recipe, say 'Bon Appétit!' " 
 
-      user_message = Message.new
-      user_message.recipe_id = the_recipe.id
-      user_message.role = "user"
-      user_message.content = "Can you suggest a recipe that contains #{the_recipe.ingredient}?"
-      user_message.save
+        system_message.save
 
-      client = OpenAI::Client.new(access_token: ENV.fetch("GPT4_KEY"))
+        user_message = Message.new
+        user_message.recipe_id = the_recipe.id
+        user_message.role = "user"
+        user_message.content = "Can you suggest a recipe that contains #{the_recipe.ingredient}?"
+        user_message.save
 
-      api_messages_array = Array.new
+        client = OpenAI::Client.new(access_token: ENV.fetch("GPT4_KEY"))
 
-      recipe_messages = Message.where({ :recipe_id => the_recipe.id }).order(:created_at)
+        api_messages_array = Array.new
 
-      recipe_messages.each do |the_message|
-        message_hash = { :role => the_message.role, :content => the_message.content }
+        recipe_messages = Message.where({ :recipe_id => the_recipe.id }).order(:created_at)
 
-        api_messages_array.push(message_hash)
-      end 
+        recipe_messages.each do |the_message|
+          message_hash = { :role => the_message.role, :content => the_message.content }
 
-      response = client.chat(
-        parameters: {
-            model: "gpt-3.5-turbo",
-            messages: api_messages_array,
-            temperature: 1.0,
-        }
-      )
+          api_messages_array.push(message_hash)
+        end 
 
-      assistant_message = Message.new
-      assistant_message.recipe_id = the_recipe.id
-      assistant_message.role = "assistant"
-      assistant_message.content = response.fetch("choices").at(0).fetch("message").fetch("content")
-      assistant_message.save
-      
-      redirect_to("/recipes/#{the_recipe.id}", { :notice => "Recipe created successfully." })
+        response = client.chat(
+          parameters: {
+              model: "gpt-3.5-turbo",
+              messages: api_messages_array,
+              temperature: 1.0,
+          }
+        )
 
+        assistant_message = Message.new
+        assistant_message.recipe_id = the_recipe.id
+        assistant_message.role = "assistant"
+        assistant_message.content = response.fetch("choices").at(0).fetch("message").fetch("content")
+        assistant_message.save
+        
+        redirect_to("/recipes/#{the_recipe.id}", { :notice => "Recipe created successfully." })
+
+      else
+        redirect_to("/recipes", { :alert => the_recipe.errors.full_messages.to_sentence })
+      end
     else
-      redirect_to("/recipes", { :alert => the_recipe.errors.full_messages.to_sentence })
+      redirect_to("/", { :alert => "You must first sign up or sign in to use the generator." })
     end
   end
 end
